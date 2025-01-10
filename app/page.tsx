296acc64cbe38a79f4a5b0e76c3abc382d018d51
Henry Odunsi <henryodunsi05@gmail.com>
2025-01-10 21:07:26 +0100

    cart

--- app/page.tsx
+++ app/page.tsx
@@ -8,29 +8,58 @@ import Meringue from '../public/assets/images/image-meringue-thumbnail.jpg'
 import Panna from '../public/assets/images/image-panna-cotta-thumbnail.jpg'
 import Tiramisu from '../public/assets/images/image-tiramisu-thumbnail.jpg'
 import Waffle from '../public/assets/images/image-waffle-thumbnail.jpg'
+import Cancel from '../public/assets/images/icon-remove-item.svg'
+import Image from "next/image";
 
 
 
 export default function Home() {
   return (
   <>
-  <section>
-    <main>
+  <section className="bg-primary w-[80%] flex justify-center m-auto rounded-[15px] shadow-[1px_10px_10px_#000]">
+    <main className="w-[80%" >
       <h1>Desserts</h1>
-      <div>
-        <Dish dishes={Baklava} />
-        <Dish dishes={Brownie} />
-        <Dish dishes={Cake} />
-        <Dish dishes={Creme} />
-        <Dish dishes={Macaron} />
-        <Dish dishes={Meringue} />
-        <Dish dishes={Panna} />
-        <Dish dishes={Tiramisu} />
+      <div className="grid grid-cols-3 gap-10">
+        <div>
         <Dish dishes={Waffle} />
+        </div>
+        <div>
+        <Dish dishes={Creme} />
+        </div>
+        <div>
+        <Dish dishes={Macaron} />
+        </div>
+        <div>
+        <Dish dishes={Tiramisu} />
+        </div>
+        <div>
+          <Dish dishes={Baklava} />
+        </div>
+        <div>
+        <Dish dishes={Meringue} />
+        </div>
+        <div>
+        <Dish dishes={Cake} />
+        </div>
+        <div>
+        <Dish dishes={Brownie} />
+        </div>
+        <div>
+        <Dish dishes={Panna} />
+        </div>
       </div>
     </main>
-    <menu>
-
+    <menu className="w-[20%]">
+      <h1>Your Cart (7)</h1>
+      <div className="flex flex-row               justify-between">
+        <div>
+        <h2>Classic Tiramisu</h2>
+        <p><span>1x</span> @ $5.50 $5.50</p>
+        </div>
+        <div >
+        <Image className="rounded-[50%] border-gray-900 border-[1px]" src={Cancel} alt="unk" />
+        </div>
+      </div>
     </menu>
   </section>
   </>
